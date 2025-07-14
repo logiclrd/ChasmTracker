24901d9dac66ad375abdcbb0cc4f39924661de37
Jonathan Gilbert <logic@deltaq.org>
2025-07-14 12:34:52 -0500

    Translated f2r.c.

---
+++ FileTypes/Converters/F2R.cs
@@ -0,0 +1,35 @@
+using System.IO;
+
+namespace ChasmTracker.FileTypes.Converters;
+
+using ChasmTracker.FileSystem;
+using ChasmTracker.Utility;
+
+public class F2R : FileInfoReader
+{
+	/* --------------------------------------------------------------------- */
+
+	/* TODO: test this code */
+
+	public override bool ReadInfo(Stream stream, FileReference file)
+	{
+		byte[] magic = new byte[3];
+
+		stream.ReadExactly(magic);
+
+		if (magic.ToStringZ() != "F2R")
+			return false;
+
+		byte[] title = new byte[40];
+
+		stream.Position += 3;
+		stream.ReadExactly(title);
+
+		file.Description = "Farandole 2 (linear)";
+		/*file.Extension = "f2r";*/
+		file.Title = title.ToStringZ();
+		file.Type = FileSystem.FileTypes.ModuleS3M;
+
+		return true;
+	}
+}
